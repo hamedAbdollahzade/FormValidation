da7a6ac93b933ed85d735e44fb18800700f921b3
Hame:D <hamedsmart20@gmail.com>
2023-06-26 16:34:01 +0330

    add function validatPhone  registery

---
+++ js/registery_js.js
@@ -0,0 +1,15 @@
+    // document.getElementById('span-tel').style.display='none';
+    // document.querySelectorAll('span')[0].style.display='none'
+    function validatPhone(event) {
+        // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
+        document.querySelectorAll('span')[0].style.display = ''
+        let phone = event.target.value;
+        if ( phone.length == 11 )
+        {
+            for (let i = 0; i < 11 ; i++) {   
+                if( phone[i] >= 0 && phone[i] <= 9)
+                {document.querySelectorAll('span')[0].style.display = 'none';}
+            }
+        }
+
+    }
