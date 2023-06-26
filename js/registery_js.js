aa10aaf0b0fbf4353f3aae57abd914262904311b
Hame:D <hamedsmart20@gmail.com>
2023-06-26 18:57:08 +0330

    controler filde Repeat Pass2

--- js/registery_js.js
+++ js/registery_js.js
@@ -1,4 +1,4 @@
-//! ------------------ add control filde mobile ------------------------------------
+//! ------------------ add controler filde mobile ------------------------------------
     function validatPhone(event) {
         // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
         document.querySelectorAll('span')[0].style.display = '';
@@ -11,19 +11,22 @@
             }
         }
     }
-//! ------------------- end of control filde mobile -------------------------------
-//? ------------------- start control filde pass 1 --------------------------------
+//! ------------------- end of controler filde mobile -------------------------------
 
-    function validatPass(event)
-    {
+//? ------------------- start controler filde pass 1 --------------------------------
+    function validatPass(event) {
         document.querySelectorAll('span')[1].style.display = '' ;
         let pass = event.target.value ;
-        if (pass.length == 8){
-            document.querySelectorAll('span')[1].style.display = 'none' ;
-        }
-        
+        if (pass.length == 8){document.querySelectorAll('span')[1].style.display = 'none' ;}
     }
+//? ------------------- end controler filde pass 1 --------------------------------
 
+//! ------------------- start controler filde Repeat Pass2 -------------------------------
+    function validatRepeatPass(event) {
 
-
-//? ------------------- end control filde pass 1 --------------------------------
+        document.querySelectorAll('span')[2].style.display = ''; 
+        let p1 = document.getElementById('pass_input1').value ;
+        let p2 = document.getElementById('pass_input2').value ;
+        if (p1 == p2) {document.querySelectorAll('span')[2].style.display = 'none';}
+    }
+//! ------------------- End controler filde Repeat Pass2 -------------------------------
