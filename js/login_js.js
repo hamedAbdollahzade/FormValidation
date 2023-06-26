1534b98d8899a7b6078ddbae173b066f7371e23d
Hame:D <hamedsmart20@gmail.com>
2023-06-26 20:03:40 +0330

    add controler filde mobile for login page

--- js/login_js.js
+++ js/login_js.js
@@ -1,1 +1,14 @@
-alert("test login javascript") ;
+//! ------------------ add controler filde mobile ------------------------------------
+function validatPhone(event) {
+    // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
+    document.querySelectorAll('span')[0].style.display = '';
+    let phone = event.target.value;
+    if ( phone.length == 11 )
+    {
+        for (let i = 0; i < 11 ; i++) {   
+            if( phone[i] >= 0 && phone[i] <= 9)
+            {document.querySelectorAll('span')[0].style.display = 'none';}
+        }
+    }
+}
+//! ------------------- end of controler filde mobile -------------------------------
