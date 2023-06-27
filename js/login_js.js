91fb4515686307d56d5f01f18f2531a8fc237cbb
Hame:D <hamedsmart20@gmail.com>
2023-06-27 13:51:24 +0330

    Debug controller phone - login form

--- js/login_js.js
+++ js/login_js.js
@@ -3,11 +3,14 @@ function validatPhone(event) {
     // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
     document.querySelectorAll('span')[0].style.display = '';
     let phone = event.target.value;
-    if ( phone.length == 11 )
+    if(Number(phone))
     {
-        for (let i = 0; i < 11 ; i++) {   
-            if( phone[i] >= 0 && phone[i] <= 9)
-            {document.querySelectorAll('span')[0].style.display = 'none';}
+        if ( phone.length == 11 )
+        {
+            for (let i = 0; i < 11 ; i++) {   
+                if( phone[i] >= 0 && phone[i] <= 9)
+                {document.querySelectorAll('span')[0].style.display = 'none';}
+            }
         }
     }
 }
