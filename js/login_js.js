5370ccd658c04b40c812b20df23bb030e9f8ec6a
Hame:D <hamedsmart20@gmail.com>
2023-06-27 19:24:52 +0330

    update controler mobile login page (09)

--- js/login_js.js
+++ js/login_js.js
@@ -2,16 +2,17 @@
 function validatPhone(event) {
     // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
     document.querySelectorAll('span')[0].style.display = '';
+    document.querySelector('form input').style.border = '2px red solid'
     let phone = event.target.value;
-    if(Number(phone))
+    if( Number(phone) && phone[0]== 0 && phone[1]== 9 && phone.length == 11)
     {
-        if ( phone.length == 11 )
-        {
             for (let i = 0; i < 11 ; i++) {   
                 if( phone[i] >= 0 && phone[i] <= 9)
-                {document.querySelectorAll('span')[0].style.display = 'none';}
-            }
-        }
+                {
+                    document.querySelectorAll('span')[0].style.display = 'none';
+                    document.querySelector('form input').style.border = ''
+                }
+            }   
     }
 }
 //! ------------------- end of controler filde mobile -------------------------------
