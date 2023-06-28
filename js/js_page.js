17db023ce1a0e8d136b6fafcbbfeda0bbbd7fce8
Hame:D <hamedsmart20@gmail.com>
2023-06-28 03:44:34 +0330

    Bug fix and  & Update to version2

--- js/js_page.js
+++ js/js_page.js
@@ -1,12 +1,9 @@
 //! ------------------ add controler filde mobile ------------------------------------
-    function validatPhone(event) {
-        // گفتم حالت پیش فرض این باشه ک اشتباهه مگه خلافش ثابت بشه 
-        document.querySelectorAll('span')[0].style.display = '';
-        document.querySelectorAll('form input')[0].style.border = '2px red solid';
+    function validatPhone(event) { 
             let phone = event.target.value;
-            // debugger
+        //debugger
         // اینجا میام سه حالت اینکه فقط عدد باشه و با صفر و نه شرووع بشه رو بررسی میکنم بعد وارد فاز بعدی میشم ک همه کارکتر ها عدد باشن 
-            if ( Number(phone) && phone[0] == 0 && phone[1] == 9 )
+            if ( Number(phone) && phone[0] == 0 && phone[1] == 9 && phone.length == 11 )
             {
                         for (let i = 0; i < 11 ; i++) {   
                             if( phone[i] >= 0 && phone[i] <= 9)
@@ -14,32 +11,52 @@
                                 document.querySelectorAll('span')[0].style.display = 'none';
                                 document.querySelectorAll('form input')[0].style.border = '';
                             }
+                            else
+                            {
+                                document.querySelectorAll('span')[0].style.display = '';
+                                document.querySelectorAll('form input')[0].style.border = '2px red solid';
+                                // اینجا بریک پوینت گذاشتم چون تو دیباگ کردن فهمیدم یازده بار شرط رو چک میکنه حتی اگه اشتباه باشه 
+                                break;
+                            }
                         }      
             }
+            else
+                {
+                    document.querySelectorAll('span')[0].style.display = '';
+                    document.querySelectorAll('form input')[0].style.border = '2px red solid';
+                }
         }
 //! ------------------- end of controler filde mobile -------------------------------
 
-//? ------------------- start controler filde pass 1 --------------------------------
-    function validatPass(event) {
-        document.querySelectorAll('span')[1].style.display = '';
-        document.querySelectorAll('form input')[1].style.border = '2px red solid';
+//? ------------------- start controler filde pass 1 -------------------registery-------------
+    function validatPassReg1(event) {
+        
             let pass = event.target.value ;
             if (pass.length == 8){
                 document.querySelectorAll('span')[1].style.display = 'none' ;
                 document.querySelectorAll('form input')[1].style.border = '';
             }
+            else
+            {
+                document.querySelectorAll('span')[1].style.display = '';
+                document.querySelectorAll('form input')[1].style.border = '2px red solid';
+            }
         }
 //? ------------------- end controler filde pass 1 --------------------------------
 
-//! ------------------- start controler filde Repeat Pass2 -------------------------------
-    function validatRepeatPass() {
-        document.querySelectorAll('span')[2].style.display = '';
-        document.querySelectorAll('form input')[2].style.border = '2px red solid'; 
+//! ------------------- start controler filde Repeat Pass2 ----------------------registery---------
+    function validatRepeatPassReg2() {
+         
             let p1 = document.getElementById('pass_input1').value ;
             let p2 = document.getElementById('pass_input2').value ;
             if (p1 == p2) {
                 document.querySelectorAll('span')[2].style.display = 'none';
                 document.querySelectorAll('form input')[1].style.border = '';
+            }
+            else
+            {
+                document.querySelectorAll('span')[2].style.display = '';
+                document.querySelectorAll('form input')[2].style.border = '2px red solid';
             }
         }
 //! ------------------- End controler filde Repeat Pass2 -------------------------------
