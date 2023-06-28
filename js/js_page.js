53d25c6116597c6ff7109910146ab5ff14a7766b
Hame:D <hamedsmart20@gmail.com>
2023-06-28 04:07:15 +0330

    comments & add alert for pass (login form)

--- js/js_page.js
+++ js/js_page.js
@@ -1,10 +1,12 @@
 //! ------------------ add controler filde mobile ------------------------------------
     function validatPhone(event) { 
-            let phone = event.target.value;
+        // این تابع رو برای بررسی صحت شماره تلفن های صفحه لاگین و ریجستری نوشتم     
+        let phone = event.target.value;
         //debugger
         // اینجا میام سه حالت اینکه فقط عدد باشه و با صفر و نه شرووع بشه رو بررسی میکنم بعد وارد فاز بعدی میشم ک همه کارکتر ها عدد باشن 
             if ( Number(phone) && phone[0] == 0 && phone[1] == 9 && phone.length == 11 )
-            {
+            { 
+                // دستور فور تا کوچکتر مساوی یازده رقم وارد میکنم چون از صفر شروع میشه
                         for (let i = 0; i < 11 ; i++) {   
                             if( phone[i] >= 0 && phone[i] <= 9)
                             {
@@ -20,6 +22,7 @@
                             }
                         }      
             }
+            // این الس رو گذاشتم چون اگه کاربر حتی با رقم صفر و نه وارد کنه ولی طولش کمتر از یازده رقم باشه بازم پیغام خطا نشون نمیده چون وارد حلقه بعدی نمیشه 
             else
                 {
                     document.querySelectorAll('span')[0].style.display = '';
@@ -30,7 +33,7 @@
 
 //? ------------------- start controler filde pass 1 -------------------registery-------------
     function validatPassReg1(event) {
-        
+        // ی تابع نوشتم برای اعتبار سنجی پسورد ک چک کنه ببینه هشت  رقم وارد شده باشه 
             let pass = event.target.value ;
             if (pass.length == 8){
                 document.querySelectorAll('span')[1].style.display = 'none' ;
